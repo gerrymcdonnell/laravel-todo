0bf4a926752e4854c36382ca5382d49f6195da9f
Gerry Mc Donnell <irishbloke@gmail.com>
2018-02-14 16:08:45 +0000

    added template file

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel first app</title>
+
+        <!-- add app css file (auto looks in public folder) -->
+        <link rel="stylesheet" href="/css/app.css">
+
+
+    </head>
+
+    <body>
+
+        <!-- content goes here -->
+
+<div class="container">
+
+    <div class="row">
+        <div class="col-md-8 col-lg-8">
+            @yield('content')
+        </div>
+
+        <div class="col-md-4 col-lg-4">
+
+        </div>
+    </div>
+
+</div>
+
+    <footer id="footer" class="text-center">
+        <p>Copyright &; 2018</p>
+    </footer>
+
+    </body>
+
+
+</html>
